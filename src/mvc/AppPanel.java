feadb90c73d6bbac0f9827172044a5bb44701b43
Jesse2131 <6jessed6@gmail.com>
2021-03-15 16:07:39 -0700

    AppPanel Update 1.0

--- src/mvc/AppPanel.java
+++ src/mvc/AppPanel.java
@@ -24,10 +24,29 @@ public class AppPanel extends JPanel implements ActionListener, PropertyChangeLi
     public static int FRAME_HEIGHT = 300;
     public AppPanel(AppFactory factory) {
         controlPanel = new JPanel();
-        model = new Model();
+
         af = factory;
+
+        af.makeView(af.makeModel());
         JPanel controlPanel = new JPanel();
         view = new View(model);
+
+        setLayout((new GridLayout(1, 2)));
+        add(controlPanel);
+        add(view);
+
+        controlPanel.setBackground(Color.PINK);
+        view.setBackground(Color.WHITE);
+
+
+
+        frame = new JFrame();
+        Container cp = frame.getContentPane();
+        cp.add(this);
+        frame.setJMenuBar(createMenuBar());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle(af.getTitle());
+        frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
     }
 
     protected JMenuBar createMenuBar() {
@@ -50,12 +69,74 @@ public class AppPanel extends JPanel implements ActionListener, PropertyChangeLi
 
     public void display() { frame.setVisible(true); }
 
+    public void propertyChange(PropertyChangeEvent pe){}
+
     public void actionPerformed(ActionEvent ae) {
+        String cmmd = ae.getActionCommand();
+        /*if (cmmd == "Save") {
+            model.setUnsavedChanges(false);
+            try {
+                //String fName = Utilities.ask("File Name?");
+                String fName = Utilities.getFileName(null, false);
+                ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(fName));
+                os.writeObject(model);
+                os.close();
+            } catch (Exception err) {
+                Utilities.error(err);
+            }
+        } else if (cmmd == "Open") {
+            if(model.getUnsavedChanges()) {
+                if (!Utilities.confirm("Open without saving?")) {
+                    try {
+                        String fName = Utilities.getFileName(null, true);
+                        ObjectInputStream is = new ObjectInputStream(new FileInputStream(fName));
+                        //model.removePropertyChangeListener(this);
+                        model = (Turtle) is.readObject();
+                        //this.model.initSupport();
+                        //model.addPropertyChangeListener(this);
+                        view.setModel(model);
+                        is.close();
+                    } catch (Exception err) {
+                        Utilities.error(err.getMessage());
+                    }
+                }
+            } else{
+                try {
+                    String fName = Utilities.getFileName(null, true);
+                    ObjectInputStream is = new ObjectInputStream(new FileInputStream(fName));
+                    //model.removePropertyChangeListener(this);
+                    model = (Turtle)is.readObject();
+                    //this.model.initSupport();
+                    //model.addPropertyChangeListener(this);
+                    view.setModel(model);
+                    is.close();
+                } catch (Exception err) {
+                    Utilities.error(err.getMessage());
+                }
+            }
+        } else if (cmmd == "New") {
+            model = new Turtle();
+            view.setModel(model);
+        } else if (cmmd == "Quit") {
+            //Utilities.saveChanges(model);
+            if(view.getUnsavedChanges()){
+                if(!Utilities.confirm("Exit without saving?")){
+                    System.exit(1);
+                }
+            }
+            else{
+                System.exit(1);
+            }
 
+        } else if (cmmd == "About") {
+            Utilities.inform(view.getAbout());
+        } else if (cmmd == "Help") {
+            Utilities.inform("Click the buttons to move the turtle. \nPen toggles whether or not to draw. \nClear removes the lines\nTo Select a color use the color button");
+        }  else  {
+            Utilities.error("Unrecognized command: " + cmmd);
+        }
+        *
+         */
     }
 
-    @Override
-    public void propertyChange(PropertyChangeEvent evt) {
-
-    }
 }
