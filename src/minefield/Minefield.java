0925a201bf8c32b34942c581881d46f18d4226d0
Jesse2131 <6jessed6@gmail.com>
2021-03-17 14:36:32 -0700

    combine square and minefield

--- src/minefield/Minefield.java
+++ src/minefield/Minefield.java
@@ -7,7 +7,25 @@ import java.util.*;
 
 public class Minefield extends Model { //The minefield is a 20x20 grid
 
+    class Square {
+        private boolean hasMine, discoved; //Whether or not contains mine and whether or not person has stepped on.
+
+        public Square(boolean hasMine, boolean discoved){
+            this.hasMine = hasMine;
+            this.discoved = discoved;
+        }
+
+        public void getNearbyMines(){}
+
+        public boolean getDiscovered(){
+            return discoved;
+        }
+
+    }
+
     private Square[][] minefield = new Square[20][20];
+
+
 
     public Square[][] getMineField() {
         return minefield;
