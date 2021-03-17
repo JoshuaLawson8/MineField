a9dcb6225c68dc7306c5967e2997329bb3c9f7f3
Josh <joshualawson8@gmail.com>
2021-03-17 13:43:42 -0700

    Construction of controlPanel for minefield

---
+++ src/minefield/MinefieldPanel.java
@@ -0,0 +1,28 @@
+package minefield;
+
+import mvc.AppFactory;
+import mvc.AppPanel;
+
+import javax.swing.*;
+
+public class MinefieldPanel extends AppPanel{
+
+
+        public MinefieldPanel(AppFactory factory) {
+            super(factory);
+            String[] buttonNames = {"NW","N","NE","W","E","SW","S","SE"};
+            for(int i = 0; i < buttonNames.length; i++) {
+                JButton current = new JButton(buttonNames[i]);
+                current.addActionListener(this);
+                controlPanel.add(current);
+            }
+
+        }
+
+        public static void main(String[] args) {
+            //AppFactory factory = new MinefieldFactory();
+            //AppPanel panel = new stopLight.StoplightPanel(factory);
+           // panel.display();
+        }
+
+}
