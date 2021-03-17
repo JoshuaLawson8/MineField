af7976577d1cb82b25a64abc72b3e5c7f7c5d473
KungFuz1 <80294773+KungFuz1@users.noreply.github.com>
2021-03-17 14:27:34 -0700

    env MineCommand start file

---
+++ src/minefield/MineCommand.java
@@ -0,0 +1,14 @@
+package minefield;
+
+import mvc.*;
+
+public class MineCommand extends Command{
+
+    protected Minefield minefield;
+
+    public MineCommand(Model model){super(model);}
+    public void execute(){
+
+    }
+}
+
