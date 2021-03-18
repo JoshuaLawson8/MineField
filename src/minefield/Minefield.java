20f95e642ba28bba4f31350b97380e80ed02e17c
Jesse2131 <6jessed6@gmail.com>
2021-03-17 20:21:46 -0700

    working on near mines

--- src/minefield/Minefield.java
+++ src/minefield/Minefield.java
@@ -2,22 +2,36 @@ package minefield;
 
 import mvc.*;
 
-import java.awt.Color;
+import javax.swing.*;
 import java.util.*;
 
-class Square {
-    private boolean hasMine, discoved; //Whether or not contains mine and whether or not person has stepped on.
-    private int mines;
-    public Square(boolean hasMine, boolean discoved, int mines){
+class Square extends JLabel {
+    private boolean hasMine, discovered, isExit; //Whether or not contains mine and whether or not person has stepped on.
+    private int nearMines;
+    public Square(boolean hasMine, boolean discovered, boolean isExit){
         this.hasMine = hasMine;
-        this.discoved = discoved;
-        this.mines = mines;
+        this.discovered = discovered;
+        this.isExit = isExit;
     }
 
-    public void getNearbyMines(){}
+    public void setNearbyMines(int nearMines){
+        this.nearMines = nearMines;
+    }
+
+    public int getNearMines(){
+        return nearMines;
+    }
+
+    public String toString(){
+        return "Has Mine: " + hasMine;
+    }
+
+    public boolean hasMine(){
+        return hasMine;
+    }
 
     public boolean getDiscovered(){
-        return discoved;
+        return discovered;
     }
 
 }
@@ -28,19 +42,80 @@ public class Minefield extends Model { //The minefield is a 20x20 grid
 
     public Minefield(){
         minefield = new Square[20][20];
-        minefield[0][0] = new Square(false,true, 0); //starter square
-        for(int i=1; i<minefield.length; i++){
-            for(int j=1; j<minefield[i].length; j++){
+        for(int i=0; i<minefield.length; i++){
+            for(int j=0; j<minefield[i].length; j++){
                 if(rd.nextBoolean() == true){
-                    minefield[i][j] = new Square(true,false, 1);
+                    minefield[i][j] = new Square(true,false, false);
                 }
                 else{
-                    minefield[i][j] = new Square(false,false, 0);
+                    minefield[i][j] = new Square(false,false, false);
                 }
             }
         }
-
+        minefield[0][0] = new Square(false,true, false); //starter square
+        minefield[19][19] = new Square(false, false, true); //exit square
     }
+
+    public void printAllMines(){
+        String txt = "";
+        for(int i=0; i<minefield.length; i++){
+            for(int j=0; j<minefield[i].length; j++){
+                txt += minefield[i][j].toString() + " ";
+            }
+            txt += "\n";
+        }
+        System.out.println(txt);
+    }
+
+    public void findNearMines(int i, int j){
+        int nearMines = 0;
+        if(i == 0 || i == 19 || j == 0 || j == 19){ //Check Edge squares
+            if(i == 0 && j == 0){ //first square
+                if(minefield[i+1][j].hasMine()){
+                    nearMines++;
+                }
+            }
+            else if(i == 19 && j == 19){ //last square
+
+            }
+            else if(i == 0){
+
+            }
+            else if(i == 19){ //Bottom row
+
+            }
+            else if(j == 0){ //Left column
+
+            }
+            else if(j == 19){ //Right colum
+
+            }
+        }
+        else{ //Check Middle squares
+            if(minefield[i+1][j].hasMine()){
+                nearMines++;
+            }
+            else if(minefield[i-1][j].hasMine()){
+                nearMines++;
+            }
+            else if(minefield[i][j+1].hasMine()){
+                nearMines++;
+            }
+            else if(minefield[i][j-1].hasMine()){
+                nearMines++;
+            }
+            else if(minefield[i+1][j+1].hasMine()){
+                nearMines++;
+            }
+            else if(minefield[i+1][j+1].hasMine()){
+                nearMines++;
+            }
+        }
+
+        minefield[i][j].setNearbyMines(nearMines);
+    }
+
+
 
     public Square[][] getMineField() {
         return minefield;
@@ -51,4 +126,6 @@ public class Minefield extends Model { //The minefield is a 20x20 grid
     public void change() {
         changed(); // from Model, sets changed flag and fires changed event
     }
+
+
 }
