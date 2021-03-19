8725c48febdf3e768479a72c67b5f181e12def91
KungFuz1 <80294773+KungFuz1@users.noreply.github.com>
2021-03-18 22:17:59 -0700

    Ethan V propertychange() update

--- src/minefield/MinefieldView.java
+++ src/minefield/MinefieldView.java
@@ -8,11 +8,12 @@ import java.awt.*;
 import java.beans.PropertyChangeEvent;
 
 public class MinefieldView extends View {
+    Minefield mf;
 
     public MinefieldView(Minefield minefield){
         super(minefield);
         setLayout(new GridLayout(20,20));
-        Minefield mf = (Minefield) model;
+        mf = (Minefield) model;
         for(int i=0; i<mf.getMineField().length; i++){
             for(int j=0; j<mf.getMineField()[i].length; j++){
                 if(mf.getMineField()[i][j].hasMine() == true){
@@ -34,8 +35,14 @@ public class MinefieldView extends View {
 
         //System.out.println("Drawing");
     }
+
     @Override
     public void propertyChange(PropertyChangeEvent evt){
-
+        mf = mf.getField();//made get Minefield object method
+        int label = mf.getUser().nearMines;
+        JLabel patch = new JLabel(String.valueOf(label));
+        patch.setBorder(BorderFactory.createLineBorder(Color.white));
+        add(patch);
+        System.out.println("pchanged");
     }
 }
