4bf428e225f6a1d7045c93b4f676ee44ce763a93
Josh <joshualawson8@gmail.com>
2021-03-19 16:08:41 -0700

    removing paintComponent and property change just repaints

--- src/minefield/MinefieldPanel.java
+++ src/minefield/MinefieldPanel.java
@@ -4,6 +4,7 @@ import mvc.AppFactory;
 import mvc.AppPanel;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MinefieldPanel extends AppPanel{
 
@@ -13,10 +14,11 @@ public class MinefieldPanel extends AppPanel{
             String[] buttonNames = {"NW","N","NE","W","E","SW","S","SE"};
             for(int i = 0; i < buttonNames.length; i++) {
                 JButton current = new JButton(buttonNames[i]);
+                current.setPreferredSize(new Dimension(100, 40));
                 current.addActionListener(this);
                 controlPanel.add(current);
             }
-
+            controlPanel.setLayout(new FlowLayout());
         }
 
         public static void main(String[] args) {
