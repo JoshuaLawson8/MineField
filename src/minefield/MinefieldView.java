cbef5c794488768f1e65c1fbe83d7dc3fc697c91
Josh <joshualawson8@gmail.com>
2021-03-19 16:00:53 -0700

    removing paintComponent and property change just repaints

--- src/minefield/MinefieldView.java
+++ src/minefield/MinefieldView.java
@@ -14,7 +14,6 @@ public class MinefieldView extends View {
     public MinefieldView(Minefield minefield){
         super(minefield);
         setLayout(new GridLayout(20,20));
-        //We don't need to create a new minefield here, that's what the call to super does
         mf = (Minefield) model;
         for(int i=0; i<mf.getMineField().length; i++){
             for(int j=0; j<mf.getMineField()[i].length; j++){
@@ -22,20 +21,15 @@ public class MinefieldView extends View {
             }
         }
     }
-    @Override
-    public void paintComponent(Graphics gc) {
-        super.paintComponent(gc);
-
-        //System.out.println("Drawing");
-    }
 
     @Override
     public void propertyChange(PropertyChangeEvent evt){
-        mf = mf.getField();//made get Minefield object method
-        int label = mf.getUser().nearMines;
+        /*int label = mf.getUser().nearMines;
         JLabel patch = new JLabel(String.valueOf(label));
         patch.setBorder(BorderFactory.createLineBorder(Color.white));
         add(patch);
-        System.out.println("pchanged");
+        System.out.println("pchanged");*/
+        repaint();
+
     }
 }
