f40b3a6d8d1dd400ab467d975cb9482bbb50a5ba
Josh <joshualawson8@gmail.com>
2021-03-22 19:36:09 -0700

    restructuring view

--- src/minefield/MinefieldView.java
+++ src/minefield/MinefieldView.java
@@ -1,12 +1,11 @@
 package minefield;
 
-import mvc.Model;
 import mvc.View;
-import stopLight.Stoplight;
 
 import javax.swing.*;
 import java.awt.*;
 import java.beans.PropertyChangeEvent;
+
 
 public class MinefieldView extends View {
 
@@ -21,41 +20,40 @@ public class MinefieldView extends View {
         removeAll();
         setLayout(new GridLayout(20,20));
         Minefield mf = (Minefield) model;
-        for(int i=0; i<mf.getMineField().length; i++){
-            for(int j=0; j<mf.getMineField()[i].length; j++){
+        for(int i=0; i<mf.getMinefield().length; i++){
+            for(int j=0; j<mf.getMinefield()[i].length; j++){
 
                 JLabel current = new JLabel();
                 current.setHorizontalAlignment(SwingConstants.CENTER);
                 current.setOpaque(true);
                 current.setBackground(Color.gray);
+                current.setBorder(BorderFactory.createLineBorder(Color.black));
+                current.setText("?");
 
-                if(mf.getMineField()[i][j].discovered){
-                    if(mf.getMineField()[i][j].hasMine){
+                Minefield.Square currentSquare = mf.getMinefield()[i][j];
+                if(currentSquare.discovered){
+                    current.setBorder(BorderFactory.createLineBorder(Color.white));
+                    current.setText(String.valueOf(mf.getMinefield()[i][j].nearMines));
+                    if(currentSquare.hasMine){
                         current.setBorder(BorderFactory.createLineBorder(Color.pink));
                         current.setBackground(Color.pink);
                         current.setText("M");
                     }
-                    else if(!mf.getMineField()[i][j].isExit){
-                        current.setBorder(BorderFactory.createLineBorder(Color.white));
-                        current.setText(String.valueOf(mf.getMineField()[i][j].nearMines));
-                    }
-                    else{
+                }
+                if(currentSquare.isExit){
+                    current.setBorder(BorderFactory.createLineBorder(Color.green));
+                    current.setText("E");
+                }
+                if(currentSquare == mf.getMinefield()[mf.userX()][mf.userY()]){
+                    current.setBorder(BorderFactory.createLineBorder(Color.cyan));
+                    if(currentSquare.isExit){
                         current.setBorder(BorderFactory.createLineBorder(Color.orange));
                         current.setBackground(Color.orange);
                         current.setText("W");
                     }
                 }
-                else if(mf.getMineField()[i][j].isExit){
-                    current.setBorder(BorderFactory.createLineBorder(Color.green));
-                    current.setText("E");
-                }
-                else{
-                    current.setBorder(BorderFactory.createLineBorder(Color.black));
-                    current.setText("?");
-                }
                 add(current);
             }
         }
-
     }
 }
